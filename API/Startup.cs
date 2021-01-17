9e049c53548bb6102f902bbdb9827a380bd4b7e9
James Moore <jmooreweb@gmail.com>
2021-01-17 13:43:49 -0500

    End of Section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -51,9 +51,9 @@ namespace API
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                // app.UseDeveloperExceptionPage();
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             //app.UseHttpsRedirection();
